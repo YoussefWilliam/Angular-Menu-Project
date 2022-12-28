384bbe0faeb9e936a9ef52bb2794082a8b446878
YoussefWilliam <youssef.william997@gmail.com>
2022-12-28 14:45:01 +0200

    use ngFor for redering recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
-export class RecipeListComponent {}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is just a test, no big deal',
+      'https://www.bbcgoodfoodme.com/wp-content/uploads/2022/07/Gnocchi_2.jpg'
+    ),
+  ];
+
+  ngOnInit(): void {}
+}
